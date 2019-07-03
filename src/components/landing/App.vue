05268bc5b56033cf50731a05389192fd8c375591
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-03 17:10:42 +0200

    Update App.vue

--- src/components/landing/App.vue
+++ src/components/landing/App.vue
@@ -6,14 +6,3 @@
   </div>
 </template>
 
-<script>
-  import Header from './Header.vue'
-  import Footer from './Footer.vue'
-
-  export default {
-    components: {Header, Footer},
-    data() {
-      return {};
-    },
-  }
-</script>
