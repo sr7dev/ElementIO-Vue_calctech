86b46510569a5bd14b5c984560b7fee2d948b4a3
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-07 22:40:18 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import NProgress from 'nprogress'
-
 import store from "../store"
-
 import LandingRoutes from "./landing"
 import AdmRoutes from "./adm"
 
