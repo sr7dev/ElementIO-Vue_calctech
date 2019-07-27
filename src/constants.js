87dfd9773be4659cf46d26c1cad3c52b497c6e02
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-27 22:21:12 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,24 @@
 let constants = (() => {
   let res = {
-    
+    AppName: 'CalcTech',
+    ApiUrl: 'http://35.198.156.152/api/',
+    MediaUrl: 'http://35.198.156.152/media/',
+    SystemErrMsg: 'Извините, произошла системная ошибка',
+
+    AttachmentTypeText: 1,
+    AttachmentTypeImage: 2,
+    AttachmentTypeVideo: 3,
+
+    TaskKindQuestion: 1,
+    TaskKindGroup: 2,
+    TaskTypeOneChoice: 1,
+    TaskTypeMultipleChoice: 2,
+    TaskTypeOpen: 3,
+    TaskTypeEssay: 4,
+
+    switchLabelIcon: {
+      dataOn: '\u2713',
+      dataOff: '\u2715',
     },
   };
 
